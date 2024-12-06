380bdcc9cc891d50e1d4215eda47deed119750bd
SaidakbarX <khayumkhujayev6937@gmail.com>
2024-12-06 17:56:00 +0500

    chala

---
+++ CrudHomeWork/CrudHomeWork/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace CrudHomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("boymurod" +
+                "");
+        }
+    }
+}
